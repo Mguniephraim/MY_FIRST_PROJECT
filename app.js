756135c9c68b2db8d66312c9b756b573a0701690
Mguniephraim <mguniephraim.em@gmail.com>
2022-05-30 18:29:53 +0200

    first commit

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const button = document.querySelector('.button');
 button.addEventListener('click', function(e) {
     alert('Coming Soon!');
     e.preventDefault();
+    button.style.backgroundColor = 'red'
 })
